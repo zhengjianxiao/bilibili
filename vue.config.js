e9b796160ee5efe80db754d17acc661e590701ca
zhengjianxiao <2724361934@qq.com>
2020-05-20 12:01:32 +0800

    dist3

--- vue.config.js
+++ vue.config.js
@@ -1,6 +1,27 @@
 
+// 在vue.config.js文件中：
 module.exports = {
-  assetsDir: 'static',
-  parallel: false,
-  publicPath: './',
-}
+  // 修改的配置
+  // 将baseUrl: '/api',改为baseUrl: '/',
+ // baseUrl: '/',3.0 被弃用改为 publicPath
+  outputDir: 'dist',
+  /* assetsPublicPath: process.env.NODE_ENV === 'production' ? '/vant-demo/' : '/',*/
+  // publicPath:'./',
+  assetsDir: 'assets',
+  //自己改的：
+  publicPath:'./',
+  // assetsPublicPath:"/",
+  devServer: {
+      stats: {
+          children: false,
+      },
+  },
+  css: {
+      loaderOptions: {
+          // 给 less-loader 传递选项,开启less的javascript选项
+          less: {
+              javascriptEnabled: true
+          }
+      }
+  }
+};
